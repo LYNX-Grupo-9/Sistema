cd698ebc92ced147341141217f3289d9b205d370
Gabriel Dantas <108305352+VMDGab@users.noreply.github.com>
2025-05-01 15:32:27 -0300

    Ajustes na responsividade do card de processos em destaques

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -8,6 +8,7 @@ import { format } from "date-fns";
 import { ptBR } from "date-fns/locale";
 import { HistoryComponent } from "../../components/HistoryComponent";
 import { Layout } from "../../components/Layout";
+import { SingleSelectComponent } from "../../components/SelectComponent";
 
 export function Home() {
 
@@ -26,18 +27,18 @@ export function Home() {
     return (
         <>
             <div className="flex h-screen w-screen">
-                <Layout/>
-                <div className="pt-[60px] ml-[13%] absolute h-full">
+                <Layout />
+                <div className="pt-[60px] ml-[13%] absolute h-full w-[84%]">
                     <div className="flex justify-between mb-[20px] pr-[20px]">
                         <span className="typography-black text-[var(--color-blueDark)] text-[40px]">Visão geral</span>
                         <Search />
                     </div>
                     <div className="flex w-full h-[90%]">
-                        <div>
+                        <div className="w-[70%]">
                             <div className="bgGlass h-[60%] w-[100%]">
                                 <div className="flex justify-between">
                                     <span className="typography-black text-[var(--color-blueDark)] text-[28px] ">Processos em destaque</span>
-                                    <DropdownComponent />
+                                    <SingleSelectComponent />
                                 </div>
 
                                 <div className="h-[1px] w-full bg-[var(--lineSeparator)] rounded-2xl mt-[16px] mb-[16px]"></div>
@@ -49,7 +50,7 @@ export function Home() {
                                         <span className="typography-medium text-[10px] text-[var(--grayText)] w-[150px] ">Data de inicio</span>
                                         <span className="typography-medium text-[10px] text-[var(--grayText)] w-[150px] ">Previsão de conclusão</span>
                                     </div>
-                                    <div className="overflow-y-auto h-[75%]">
+                                    <div className="overflow-y-auto h-[75%] w-full">
                                         <HighlightedCases
                                             idCase="1321321"
                                             Customer="Paulo Cesar Dantas"
@@ -140,7 +141,7 @@ export function Home() {
                             <div className="mt-[24px] flex h-[35%]">
                                 <div className="bgGlass w-[50%] mr-[12px]">
                                     <span className="typography-black text-[var(--color-blueDark)] text-[28px] ">Notificação</span>
-                                    <div className="h-[200px] overflow-y-auto py-2.5">
+                                    <div className="h-[80%] overflow-y-auto py-2.5">
                                         <OverviewNotification message="Nova consulta" />
                                         <OverviewNotification message="Nova consulta" />
                                         <OverviewNotification message="Nova consulta" />
@@ -171,18 +172,27 @@ export function Home() {
                                 </div>
                             </div>
                         </div>
-                        <div className="mx-[24px]">
+                        <div className="mx-[24px] w-[30%]">
                             <div className="bgGlass h-[30%] mb-[24px] flex flex-col">
-                                <span className="typography-black text-[var(--color-blueDark)] text-[28px]">{today}</span>
-                                <span className="typography-semibold text-[var(--grayText)] text-[20px]">Sem eventos hoje</span>
+                                <span className="typography-black text-[var(--color-blueDark)] text-lg sm:text-xl md:text-2xl lg:text-3xl">
+                                    {today}
+                                </span>
+                                <span className="typography-semibold text-[var(--grayText)] text-base sm:text-lg md:text-xl">
+                                    Sem eventos hoje
+                                </span>
                                 <div className="h-[1px] w-full bg-[var(--lineSeparator)] rounded-2xl mt-[16px] mb-[16px]"></div>
-                                <span className="typography-semibold text-[var(--color-blueDark)] text-[24px]">Próximo evento</span>
-                                <span className="typography-regular text-[var(--grayText)] text-[20px]">Atendimento Walace - 10 de março 2025</span>
+                                <span className="typography-semibold text-[var(--color-blueDark)] text-lg sm:text-xl md:text-2xl">
+                                    Próximo evento
+                                </span>
+                                <span className="typography-regular text-[var(--grayText)] text-base sm:text-lg md:text-xl">
+                                    Atendimento Walace - 10 de março 2025
+                                </span>
                             </div>
+
                             <div className="bgGlass h-[65%]">
                                 <div className="flex justify-between items-center">
                                     <span className="typography-black text-[var(--color-blueDark)] text-[28px]">Histórico</span>
-                                    <DropdownComponent />
+                                    <SingleSelectComponent />
                                 </div>
                                 <div className="flex w-full pt-[20px]">
                                     <span className="text-[var(--grayText)] typography-medium text-[12px]">Nome do cliente</span>
