73620385b49fbc10726fb4e58db210a19c48be95
henrique-olivier <henrique.rosa@sptech.school>
2025-05-12 19:37:09 -0300

    wip

---
+++ src/components/FormNewEvent/index.jsx
@@ -0,0 +1,10 @@
+export function FormNewEvent() {
+
+    return (
+        <>
+            <div>
+
+            </div>
+        </>
+    )
+}
--- src/pages/Agenda/index.jsx
+++ src/pages/Agenda/index.jsx
@@ -10,6 +10,8 @@ import getDay from 'date-fns/getDay';
 import 'react-big-calendar/lib/css/react-big-calendar.css';
 import { ptBR } from 'date-fns/locale';
 import '../../global/calendar.css';
+import { useState } from 'react';
+import { NewItemButton } from '../../components/Buttons/NewItemButton';
 
 
 // Configurações de localização (pt-BR)
@@ -29,7 +31,7 @@ const localizer = dateFnsLocalizer({
 // Eventos de exemplo
 const events = [
     {
-        title: 'Reunião com o time',
+        title: 'Reunião com o time ',
         start: new Date(2025, 4, 10, 10, 0),
         end: new Date(2025, 4, 10, 11, 30),
         allDay: false,
@@ -39,9 +41,20 @@ const events = [
         start: new Date(2025, 4, 11, 12, 0),
         end: new Date(2025, 4, 11, 13, 0),
     },
+    {
+        title: 'Nome do evento',
+        start: new Date(2025, 4, 12, 9, 30),  // 12 de maio de 2025, 09:30
+        end: new Date(2025, 4, 12, 14, 0),    // 12 de maio de 2025, 11:00
+        allDay: false, // ou true, se for o dia inteiro
+    }
 ];
 
 export default function Agenda() {
+
+    const [currentDate, setCurrentDate] = useState(new Date());
+    const [currentView, setCurrentView] = useState('month');
+
+
     return (
         <>
             <div className="flex h-[99vh] w-full">
@@ -67,14 +80,28 @@ export default function Agenda() {
                     </div>
                 </div>
                 <div className='bg-white h-screen w-full'>
-                    <div className='px-5 pt-24'>
+                    <div className='px-5 pt-14 flex flex-col gap-4'>
+                        <div className='self-end'>
+                            <NewItemButton title="Novo evento" />
+                        </div>
                         <Calendar
                             localizer={localizer}
                             events={events}
-                            defaultView="month"
-                            views={['month']}
-                            style={{ minHeight: 800 }}
+                            date={currentDate}
+                            view={currentView}
+                            onView={view => setCurrentView(view)}
+                            onNavigate={date => setCurrentDate(date)}
+                            views={['month', 'day']}
                             culture="pt-BR"
+                            style={{ height: 800 }}
+                            messages={{
+                                month: 'Mês',
+                                week: 'Semana',
+                                day: 'Dia',
+                                today: 'Hoje',
+                                previous: 'Anterior',
+                                next: 'Próximo',
+                            }}
                         />
                     </div>
                 </div>
