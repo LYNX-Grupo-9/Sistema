cd9b31a30f58a47f7633dea7f1d56fb5a44163c9
AnthonyLourenco <anthony.batista@sptech.school>
2025-05-14 10:11:59 -0300

    sobre-mobile

--- src/pages/Institucional/index.jsx
+++ src/pages/Institucional/index.jsx
@@ -1,7 +1,36 @@
+import logo from '../../assets/temis-logo.png';
+import menu from '../../assets/menu.svg';
+
 export function Institucional() {
     return (
         <div>
-            
+            <div className="sobre">
+                <div className="header">
+                    <div className='containerLogo'>
+                        <img src={logo} alt="Logo"  width="70%" height="70%" />
+                        <p>
+                            TemisHub
+                        </p>
+                    </div>
+                    <div className='containerMenu'>
+                        <img src={menu} alt="menu" />
+                    </div>    
+                </div>
+                <div className="containerText">
+                    <h1>
+                        Eficiência, controle e inteligência 
+                        para escritórios de advocacia 
+                        modernos.
+                    </h1>
+                    <p>
+                        Nosso sistema jurídico integra gestão de 
+                        clientes e processos, agenda automatizada 
+                        e um chat com inteligência artificial para 
+                        oferecer agilidade, organização e suporte 
+                        estratégico ao seu dia a dia jurídico.
+                    </p>
+                </div>
+            </div>
         </div>
     );
 }
